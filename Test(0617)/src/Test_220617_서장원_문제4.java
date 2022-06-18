1eb93a57a43a64031c0809d821e0f6758c6defd2
Lamia9304 <lamia9304@naver.com>
2022-06-19 01:20:30 +0900

    수정

--- Test(0617)/src/Test_220617_서장원_문제4.java
+++ Test(0617)/src/Test_220617_서장원_문제4.java
@@ -3,69 +3,78 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Test_220617_서장원_문제4 {
-	
+
 	public static void main(String[] args) {
-		
-		
-		
-		String engRegex="[a-zA-Z]"; // 영문자
-		
+
+		String engUpperRegex = "[A-Z]"; // 대문자
+		String engLowerRegex = "[a-z]"; // 소문자
+
 		// 숫자를 판별하는 정규표현
 		String numRegex = "[0-9]";
-		
-		//특수문자(!@#$%) 판별하는 정규표현식
+
+		// 특수문자(!@#$%) 판별하는 정규표현식
 		String specRegex = "[!@#$%]";
-		
-		 String regExpPw = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9ㄱ-힣]).{8,20}$";
-		
-		Scanner sc= new Scanner(System.in);
-		
-	//	String input=sc.nextLine();
-		//boolean matcher=regExpPw.matches(input);
-		
-	//	System.out.println(matcher);
-		
-		Scanner sp = new Scanner(System.in);
-		String password = sp.next();
-		int securityLevel = 0;
-		
-		if(Pattern.matches(engRegex, password)) {
-			System.out.println(password + " : 길이 규칙 적합!");
-			if(Pattern.compile(engUpperRegex).matcher(password).find()) {
-				System.out.println("영문자가 포함되어 있으므로 보안 등급 +1 증가");
-				securityLevel++;
+
+		String regExpPw = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9ㄱ-힣]).{8,20}$";
+
+		String lengthRegex = "^[A-Za-z0-9!@#$%]{8,16}$";
+
+//		Scanner sc= new Scanner(System.in);
+
+		// String input=sc.nextLine();
+		// boolean matcher=regExpPw.matches(input);
+
+		// System.out.println(matcher);
+
+		while (true) {
+			System.out.println("패스워드 입력: ");
+			Scanner sp = new Scanner(System.in);
+			String password = sp.next();
+
+			int securityLevel = 0;
+
+			if (Pattern.matches(lengthRegex, password)) {
+				if (Pattern.compile(engUpperRegex).matcher(password).find()) {
+
+					securityLevel++;
+				}
+
+				if (Pattern.compile(engLowerRegex).matcher(password).find()) {
+
+					securityLevel++;
+				}
+
+				if (Pattern.compile(numRegex).matcher(password).find()) {
+
+					securityLevel++;
+				}
+
+				if (Pattern.compile(specRegex).matcher(password).find()) {
+
+					securityLevel++;
+				}
+
+			} else {
+				System.out.println(password + " : 영문자,숫자,특수문자 조합 8~16자 필수!");
 			}
 
-			
-			if(Pattern.compile(numRegex).matcher(password).find()) {
-				System.out.println("숫자가 포함되어 있으므로 보안 등급 +1 증가");
-				securityLevel++;
+			switch (securityLevel) {
+			case 4:
+				System.out.println("안전!");
+				break;
+			case 3:
+				System.out.println("보통!");
+				break;
+			case 2:
+				System.out.println("위험!");
+				break;
+			case 1:
+				System.out.println("사용 불가능한 패스워드!");
+				break;
 			}
-			
-			if(Pattern.compile(specRegex).matcher(password).find()) {
-				System.out.println("특수문자가 포함되어 있으므로 보안 등급 +1 증가");
-				securityLevel++;
-			}
-			
-			
-		} else {
-			System.out.println(password + " : 길이 규칙 부적합!");
-		}
-		
-		System.out.println("보안 등급은 " + securityLevel + "");
-		
-		switch(securityLevel) {
-			case 4 : System.out.println("안전!"); break;
-			case 3 : System.out.println("보통!"); break;
-			case 2 : System.out.println("위험!"); break;
-			case 1 : System.out.println("사용 불가능한 패스워드!"); break;
-		}
-		
-		
-	}
-		
-	}
-	
 
-	
+		}
+
+	}
+
 }
